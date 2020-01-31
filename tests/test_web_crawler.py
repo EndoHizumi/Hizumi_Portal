09f9a56b876468edab0b103c8e571f9e56dda4c2
Endo_Hizumi <endohizumi0114@gmail.com>
2020-02-01 01:16:54 +0900

    update: web_crawlerのテストの一部が完了.利用可能状態

--- tests/test_web_crawler.py
+++ tests/test_web_crawler.py
@@ -1,6 +1,5 @@
 from web_crawler import web_crawler
 from unittest import TestCase, mock
-from unittest.mock import Mock
 from requests import Response
 from bs4 import BeautifulSoup
 
@@ -16,47 +15,70 @@ class TestWebCrawler(TestCase):
 class TestGetElement(TestWebCrawler):
 
     def test_get_specified_tag_name(self):
-        h2_tags = web_crawler.get_element(html_source=self.html_source, target_element={"tag": "h1"})
-        expect_tags = BeautifulSoup(
-            '<h1 style="text-align:center;"> Hizumi\'s portfolio</h1>', 'html.parser')
-        self.assertEqual(h2_tags, [expect_tags.h1])
+        actual_tags = web_crawler.get_element(
+            html_source=self.html_source, target_element={"tag": "h1"})
+        expect_tags = BeautifulSoup('<h1 style="text-align:center;"> Hizumi\'s portfolio</h1>', 'html.parser')
+        self.assertEqual(actual_tags, [expect_tags.h1])
+
+    def test_get_specified_class_name(self):
+        actual_tags = web_crawler.get_element(
+            html_source=self.html_source, target_element={"class": "title"})
+        expect_tags = ['PlanOS', 'ファイズフォン for Android',
+                       'ウィザードライバー for Android', 'しゃべってキバット！']
+        self.assertEqual([tag.text.strip() for tag in actual_tags], expect_tags)
+
+    def test_get_specified_id_name(self):
+        html_source = '<div id=hoge>hoge</div><div id=foo>foo</div>'
+        actual_tag = web_crawler.get_element(html_source=html_source, target_element={"id": "hoge"})
+        expect_tag = 'hoge'
+        self.assertEqual([tag.text for tag in actual_tag], [expect_tag])
+
+    def test_get_element_once(self):
+        actual_tags = web_crawler.get_element(
+            html_source=self.html_source, target_element={"class": "title"}, select_once=True)
+        expect_tags = 'PlanOS'
+        self.assertEqual(actual_tags.text.strip(), expect_tags)
 
 
 class TestGetPage(TestWebCrawler):
 
-    def test_get_page_argument_text(self):
-        h2_tags = web_crawler.get_page(html_text=self.html_source, target_element={"tag": "h1"})
-        expect_tags = BeautifulSoup(
-            '<h1 style="text-align:center;"> Hizumi\'s portfolio</h1>', 'html.parser')
-        self.assertEqual(h2_tags, [expect_tags.h1])
+    def test_get_page_no_argument(self):
+        empty_string = web_crawler.get_page()
+        self.assertEqual(empty_string, '')
 
     @mock.patch("requests.get")
-    def test_get_page_argument_url(self, mock_get):
+    def test_get_page_argument_text(self, mock_get):
+        # HTTPレスポンスのMockを作成する。
         res = Response()
         res.headers = {'Content-Type': 'text/html'}
         res.status_code = 200
-        res._content = b'{"body":{self.html_source}}'
+        res._content = f'{self.html_source}'.encode('utf-8')
         mock_get.return_value = res
 
-        h2_tags = web_crawler.get_page(url="http://localhost:8080/", target_element={"tag": "h1"})
-        expect_tags = BeautifulSoup(
-            '<h1 style="text-align:center;"> Hizumi\'s portfolio</h1>', 'html.parser')
-        self.assertEqual(h2_tags, [expect_tags.h1])
+        web_crawler.get_page(html_text=self.html_source, target_element={"tag": "h1"})
+        mock_get.assert_not_called()
 
+    @mock.patch("requests.get")
+    def test_get_page_argument_url(self, mock_get):
+        # HTTPレスポンスのMockを作成する。
+        res = Response()
+        res.headers = {'Content-Type': 'text/html'}
+        res.status_code = 200
+        res._content = f'{self.html_source}'.encode('utf-8')
+        mock_get.return_value = res
 
-class TestGetElementByClass(TestWebCrawler):
-    def test_get_element_by_id_once(self):
-        h2_tags = web_crawler.get_element_by_class(html_text=self.html_source, target_element="thumbnail", index=0).img
-        expect_tags = BeautifulSoup(
-            '<img width="500" height="250" src="./picture/Screenshot_2019-04-06_PlanOS.png" />', 'html.parser')
-        self.assertEqual(str(h2_tags), str(expect_tags))
+        web_crawler.get_page(url="http://localhost:8080/", html_text=self.html_source, target_element={"tag": "h1"})
+        mock_get.assert_called_with("http://localhost:8080/")
 
-    def test_get_element_by_id(self):
-        # 個数を数える
-        img_tags = web_crawler.get_element_by_class(html_text=self.html_source, target_element="thumbnail")
-        actual_img_num = 0
-        for img_tag in img_tags:
-            print(img_tag)
-            actual_img_num += 1
-        expect_img_num = 4
-        self.assertEqual(actual_img_num, expect_img_num)
+    @mock.patch("requests.get")
+    def test_get_page_argument_both_choose_the_url(self, mock_get):
+        # HTTPレスポンスのMockを作成する。
+        res = Response()
+        res.headers = {'Content-Type': 'text/html'}
+        res.status_code = 200
+        res._content = f'{self.html_source}'.encode('utf-8')
+        mock_get.return_value = res
+
+        web_crawler.get_page(url="http://localhost:8080/", html_text=self.html_source, target_element={"tag": "h1"})
+        mock_get.assert_called_with("http://localhost:8080/")
+
--- web_crawler/web_crawler.py
+++ web_crawler/web_crawler.py
@@ -4,37 +4,38 @@ from bs4 import BeautifulSoup
 from typing import Dict
 
 
-def get_element_by_class(url: str = "", html_text: str = "", target_element: str = "", index: int = None) -> bs4.element.Tag:
+def get_element_by_class_once(url: str = "", html_text: str = "", target_element: str = "") -> bs4.element.Tag:
     _target_element = {"class_": target_element}
-    resultElements = get_page(url, html_text, _target_element)
-    if index is not None:
-        result = resultElements[index]
-    else:
-        result = resultElements
-    return result
+    return get_page(url, html_text, _target_element, True)
 
 
-def get_element_by_id(url: str = "", html_text: str = "", target_element: str = "", index: int = None) -> bs4.element.Tag:
+def get_element_by_id_once(url: str = "", html_text: str = "", target_element: str = "") -> bs4.element.Tag:
     _target_element = {"id": target_element}
-    resultElements = get_page(url, html_text, _target_element)
-    if index is not None:
-        result = resultElements[index]
-    else:
-        result = resultElements
-    return result
+    return get_page(url, html_text, _target_element, True)
 
 
-def get_element_by_tag(url: str = "", html_text: str = "", target_element: str = "", index: int = None) -> bs4.element.Tag:
+def get_element_by_tag_once(url: str = "", html_text: str = "", target_element: str = "") -> bs4.element.Tag:
     _target_element = {"tag": target_element}
-    resultElements = get_page(url, html_text, _target_element)
-    if index is not None:
-        result = resultElements[index]
-    else:
-        result = resultElements
-    return result
+    return get_page(url, html_text, _target_element, True)
 
 
-def get_page(url: str = "", html_text: str = "", target_element: Dict[str, str] = {}) -> bs4.element.ResultSet:
+def get_element_by_class(url: str = "", html_text: str = "", target_element: str = "") -> bs4.element.ResultSet:
+    _target_element = {"class_": target_element}
+    return get_page(url, html_text, _target_element)
+
+
+def get_element_by_id(url: str = "", html_text: str = "", target_element: str = "") -> bs4.element.ResultSet:
+    _target_element = {"id": target_element}
+    return get_page(url, html_text, _target_element)
+
+
+def get_element_by_tag(url: str = "", html_text: str = "", target_element: str = "") -> bs4.element.ResultSet:
+    _target_element = {"tag": target_element}
+    return get_page(url, html_text, _target_element)
+
+
+def get_page(url: str = "", html_text: str = "", target_element: Dict[str, str] = {}, select_once: bool = False) -> bs4.element.ResultSet:
+    
     if len(url) > 0:
         html_source = requests.get(url).text
     elif len(html_text) > 0:
@@ -42,14 +43,18 @@ def get_page(url: str = "", html_text: str = "", target_element: Dict[str, str] 
     else:
         return ""
     _target_element = target_element
-    return get_element(html_source, _target_element)
+    return get_element(html_source, _target_element, select_once)
 
 
-def get_element(html_source: str, target_element: Dict[str, str]) -> bs4.element.ResultSet:
+def get_element(html_source: str, target_element: Dict[str, str], select_once: bool = False) -> bs4.element.ResultSet:
     bs = BeautifulSoup(html_source, 'html.parser')
     if 'tag' in target_element:
         tag_name = target_element['tag']
         element_text = bs.find_all(tag_name)
     else:
         element_text = bs.find_all(**target_element)
-    return element_text
+
+    if select_once:
+        return element_text[0]
+    else:
+        return element_text
