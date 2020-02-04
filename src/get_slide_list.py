a5b95b7151ca7ad9e9b945545ed3276cfe20e7f5
Endo_Hizumi <endohizumi0114@gmail.com>
2020-02-04 22:10:28 +0900

    リストの取得を実装した。

--- src/get_slide_list.py
+++ src/get_slide_list.py
@@ -1,5 +1,8 @@
-import web_crawler
+from web_crawler import web_crawler
+import requests
 
 speakerdeck_url = "https://speakerdeck.com/endohizumi/"
-print(web_crawler.get_element_by_class(
-    url=speakerdeck_url, target_element="row mb-4"))
+html_source = requests.get(speakerdeck_url).text
+slide_element = web_crawler.get_element(html_source, {"class": "row mb-4"})
+links = [slide_link.get("href") for slide_link in slide_element[0].find_all("a")]
+print(links)
