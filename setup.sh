4aad804541c99738d593dc7c178584167af271a2
Endo_Hizumi <endohizumi0114@gmail.com>
2021-01-12 22:52:25 +0900

    move to repository for get_your_deck_list

--- setup.sh
+++
@@ -1,3 +0,0 @@
-virtualenv -p python3 venv3
-source ./venv3/bin/activate
-pip install -r requirements.txt
--- src/get_slide_list.py
+++
@@ -1,8 +0,0 @@
-from web_crawler import web_crawler
-import requests
-
-speakerdeck_url = "https://speakerdeck.com/endohizumi/"
-html_source = requests.get(speakerdeck_url).text
-slide_element = web_crawler.get_element(html_source, {"class": "row mb-4"})
-links = [slide_link.get("href") for slide_link in slide_element[0].find_all("a")]
-print(links)
